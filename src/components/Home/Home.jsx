47021ad6da35eaeb64abf513c9667019a27bb694
farhansadik007 <sadik.zisan@gmail.com>
2023-04-10 20:44:52 +0600

    first section of home page added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div  className='info'>
+            <div className='text'>
+                <h2>One Step <br/>Closer To Your <br/><span>Dream Job</span></h2>
+                <p>Explore thousands of job opportunities with all the information you need. It's your future!Lets do it!!!</p>
+                <button>Get Started</button>
+            </div>
+            <div className='img'>
+                <img src="person.png" alt="" />
+            </div>
         </div>
     );
 };
